7ee6b17e6078c0fcdb5af5b592522b3fe3d6c87a
DawnSovereign <jason.t.kim2@gmail.com>
2024-02-20 17:18:14 -0800

    added a lot of features

---
+++ mole.js
@@ -0,0 +1,67 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function(){
+    setGame();
+}
+
+function setGame(){
+    //setting up the grid for the game board in the html
+    //assigning each tile an id
+    for(let i = 0; i < 9; i++){ //i goes from 0 to 8 (stops at 9)
+        //<div id="0-8"></div>
+        //Gonne use this id to know which tile was clicked on and see if the tile had the mole or not
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMole, 1000)// 0000milliseconds = 0 sceonds
+    setInterval(setPlant, 2000)//0000 milliseconds = 0 seconds
+}
+
+function getRandomTile(){
+    //math.random --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole(){
+
+    //clears the tags within the div tag when called again
+    //clears the previous mole
+    if(currMoleTile){
+        currMoleTile.innerHTML = "";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "./img/monty-mole.png";
+
+    //getting the random num
+    let num = getRandomTile();
+    if(currPlantTile && currPlantTile.id == num){
+        return;
+    }
+
+    //setting the current tile depending on the number (random tile)
+    currMoleTile = document.getElementById(num);
+    //putting the mole at the tile depending on the number
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant(){
+    if(currPlantTile){
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "./img/piranha-plant.png";
+
+    let num = getRandomTile();
+    if(currMoleTile && currMoleTile.id == num){
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
